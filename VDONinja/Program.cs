3f9fd49d57a4719f44264b9de7fa4ac41629d2d4
Andy Hawkins <andy@gently.org.uk>
2025-02-13 17:51:43 +0000

    Initial plugin skeleton

---
+++ VDONinja/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using BarRaider.SdTools;
+using System;
+
+namespace VDONinja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment this line of code to allow for debugging
+            //while (!System.Diagnostics.Debugger.IsAttached) { System.Threading.Thread.Sleep(100); }
+
+            SDWrapper.Run(args);
+        }
+    }
+}
